005566057c61283379417d982d1a371bb27a9ca9
RiteshPhadtare2004 <riteshphadtare12.02.2004@gmail.com>
2023-10-07 16:47:22 +0530

    Create Account done

--- IT_server/index.js
+++ IT_server/index.js
@@ -8,7 +8,6 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/IT');
     console.log('db connected');
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 const userSchema = new mongoose.Schema({
